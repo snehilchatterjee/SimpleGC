7579e2f671cef5014d487d75d5fc69d97de119cb
Snehil Chatterjee <snehilchatterjee07@gmail.com>
2025-03-31 20:23:38 +0530

    vec3 created

--- mooseobject.c
+++ mooseobject.c
@@ -37,3 +37,15 @@ moose_object_t *new_moose_string(char* value){
     moose_obj->data.v_string=str;
     return moose_obj;
 }
+
+moose_object_t *new_moose_vector3(moose_object_t *x, moose_object_t *y, moose_object_t *z){
+    if(x==NULL || y==NULL || z==NULL) return NULL;
+    moose_object_t* moose_obj=malloc(sizeof(moose_object_t));
+    if(moose_obj==NULL){
+        return NULL;
+    }
+    moose_obj->kind=VECTOR3;
+    moose_vector_t vec={x,y,z};
+    moose_obj->data.v_vector3=vec;
+    return moose_obj;
+}
--- mooseobject.h
+++ mooseobject.h
@@ -1,15 +1,26 @@
 #include<stddef.h>
 
+typedef struct moose_object_t moose_object_t;
+
+
+typedef struct moose_vector_t{
+    moose_object_t* x;
+    moose_object_t* y;
+    moose_object_t* z;
+} moose_vector_t;
+
 typedef enum moose_object_kind_t{
     INTEGER,
     FLOAT,
-    STRING
+    STRING,
+    VECTOR3
 } moose_object_kind_t;
 
 typedef union moose_object_data_t{
     int v_int;
     float v_float;
     char* v_string;
+    moose_vector_t v_vector3;
 } moose_object_data_t;
 
 typedef struct moose_object_t{
@@ -20,3 +31,4 @@ typedef struct moose_object_t{
 moose_object_t *new_moose_integer(int value);
 moose_object_t *new_moose_float(float value);
 moose_object_t *new_moose_string(char* value);
+moose_object_t *new_moose_vector3(moose_object_t *x, moose_object_t *y, moose_object_t *z);
