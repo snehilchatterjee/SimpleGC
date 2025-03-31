e50f7dc3e0dbd45f467f539291f2b8f50b3318bf
Snehil Chatterjee <snehilchatterjee07@gmail.com>
2025-03-31 20:05:56 +0530

    moose string implemented

--- mooseobject.c
+++ mooseobject.c
@@ -1,4 +1,5 @@
 #include "mooseobject.h"
+#include<string.h>
 
 moose_object_t *new_moose_integer(int value){
     moose_object_t* moose_obj=malloc(sizeof(moose_object_t));
@@ -17,5 +18,22 @@ moose_object_t *new_moose_float(float value){
     }
     moose_obj->kind=FLOAT;
     moose_obj->data.v_float=value;
+    return moose_obj;
+}
+
+moose_object_t *new_moose_string(char* value){
+    moose_object_t* moose_obj=malloc(sizeof(moose_object_t));
+    if(moose_obj==NULL){
+        return NULL;
+    }
+    size_t len=strlen(value);
+    char* str=malloc(len+1);
+    if(str==NULL){
+        free(moose_obj);
+        return NULL;
+    }
+    strcpy(str,value);
+    moose_obj->kind=STRING;
+    moose_obj->data.v_string=str;
     return moose_obj;
 }
--- mooseobject.h
+++ mooseobject.h
@@ -2,12 +2,14 @@
 
 typedef enum moose_object_kind_t{
     INTEGER,
-    FLOAT
+    FLOAT,
+    STRING
 } moose_object_kind_t;
 
 typedef union moose_object_data_t{
     int v_int;
     float v_float;
+    char* v_string;
 } moose_object_data_t;
 
 typedef struct moose_object_t{
@@ -17,3 +19,4 @@ typedef struct moose_object_t{
 
 moose_object_t *new_moose_integer(int value);
 moose_object_t *new_moose_float(float value);
+moose_object_t *new_moose_string(char* value);
