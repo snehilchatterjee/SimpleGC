a0e91854b0dfab29c5f35b110d5ad3241b1e5c80
Snehil Chatterjee <snehilchatterjee07@gmail.com>
2025-04-02 01:02:06 +0530

    frame functions created

--- vm.c
+++ vm.c
@@ -1,5 +1,24 @@
 #include "vm.h"
 
+void vm_frame_push(vm_t *vm,frame_t *frame){
+    if(vm==NULL || frame==NULL){
+        return;
+    }
+    stack_push(vm->frames,frame);
+}
+
+frame_t *vm_new_frame(vm_t *vm){
+    frame_t *frame=malloc(sizeof(frame_t));
+    frame->references=stack_new(8);
+    stack_push(vm->frames,frame);
+    return frame;
+}
+
+void frame_free(frame_t *frame){
+    stack_free(frame->references);
+    free(frame);
+}
+
 vm_t *vm_new(){
     vm_t *vm=malloc(sizeof(vm_t));
     vm->frames=stack_new(8);
--- vm.h
+++ vm.h
@@ -6,5 +6,14 @@ typedef struct VirtualMachine
     stack_t *objects;
 } vm_t;
 
+typedef struct StackFrame {
+    stack_t *references;
+} frame_t;
+
 vm_t *vm_new();
 void vm_free(vm_t *vm);
+
+void vm_frame_push(vm_t *vm,frame_t *frame);
+frame_t *vm_new_frame(vm_t *vm);
+
+void frame_free(frame_t *frame);
